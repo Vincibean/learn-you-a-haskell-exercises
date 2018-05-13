58ad8ec5c0eceb12defa3b466b5f2801aeaf3c92
Andrea Bessi <andrebessi00@gmail.com>
2018-05-13 16:21:01 +0000

    Create 03.answer.hs

---
+++ answerkey/ch12-a-fistful-of-monads/03.answer.hs
@@ -0,0 +1,8 @@
+{-
+ - Create a function, positiveCheck, which takes a number and returns a successful Validation if it's positive, 
+ - and a failed Validation with a String message if not.
+ -}
+positiveCheck :: (Num a, Ord a) => a -> Validation a
+positiveCheck a = if (a > 0)
+                  then Success a
+                  else Fail "negative number"
