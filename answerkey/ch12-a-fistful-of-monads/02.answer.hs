a1f3fb1b2246bdd5f4b1bac595d86374bce50aa6
Andrea Bessi <andrebessi00@gmail.com>
2018-05-13 16:19:26 +0000

    Update 02.answer.hs

--- answerkey/ch12-a-fistful-of-monads/02.answer.hs
+++ answerkey/ch12-a-fistful-of-monads/02.answer.hs
@@ -2,3 +2,13 @@
 instance Monad Validation where 
   (Success a) >>= f = f a
   (Fail s) >>= _ = Fail s
+
+instance Applicative Validation where
+  pure a = Success a
+  Success f <*> Success a = Success (f a)
+  (Fail s) <*> _ = Fail s
+  _ <*> (Fail s) = Fail s
+
+instance Functor Validation where
+  fmap f (Success a) = Success (f a)
+  fmap _ (Fail s) = Fail s
